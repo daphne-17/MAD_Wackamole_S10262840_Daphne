53b5d61cd81f6bd9ea64dd1537d177baa202915f
daphne-17 <s10262840@connect.np.edu.sg>
2026-01-21 17:50:35 +0800

    pushing advanced folder and database configurations

--- Advanced/app/src/main/java/np/ict/mad/wackamole/ui/theme/Theme.kt
+++ Advanced/app/src/main/java/np/ict/mad/wackamole/ui/theme/Theme.kt
@@ -1,26 +1,15 @@
 package np.ict.mad.wackamole.ui.theme
 
-import androidx.compose.material3.darkColorScheme
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.lightColorScheme
+import androidx.compose.runtime.Composable
 
-private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
-    secondary = PurpleGrey80,
-    tertiary = Pink80
-)
-
-private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
-    secondary = PurpleGrey40,
-    tertiary = Pink40
-
-    /* Other default colors to override
-    background = Color(0xFFFFFBFE),
-    surface = Color(0xFFFFFBFE),
-    onPrimary = Color.White,
-    onSecondary = Color.White,
-    onTertiary = Color.White,
-    onBackground = Color(0xFF1C1B1F),
-    onSurface = Color(0xFF1C1B1F),
-    */
-)
+@Composable
+fun WackAMoleTheme(
+    content: @Composable () -> Unit
+) {
+    MaterialTheme(
+        colorScheme = lightColorScheme(),
+        content = content
+    )
+}
